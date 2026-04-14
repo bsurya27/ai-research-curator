0852981ce3a278ba65b24f7ad6ecb7493e51a9f9
bsurya27 <boddusurya2003@gmail.com>
2026-04-13 23:37:06 -0400

    another minor aws change coz im an idiot

--- shutdown.sh
+++ shutdown.sh
@@ -24,7 +24,8 @@ pkill -f "python rec_model/app.py" || true
 
 # Stop EC2 instance
 echo "Stopping instance..."
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
 aws ec2 stop-instances --instance-ids $INSTANCE_ID --region $AWS_REGION
 
 echo "Done."
